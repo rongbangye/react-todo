e4d194178f8dceb40d8dc0e5f190e7d2fd9d712a
rongbangye <rongbang.ye@gmail.com>
2020-06-06 19:42:37 -0700

    Created view and edit template, also updated edit feature

--- components/App.js
+++ components/App.js
@@ -4,13 +4,29 @@ import Form from "./Form";
 import FilterButton from "./FilterButton";
 import { nanoid } from "nanoid";
 
+// Defining these constants outside our App() function
+// If defined inside it, would be recalculated every time <App /> renders. 
+const FILTER_MAP = {
+  // shows all tasks, return true for all tasks
+  All: () => true,
+  // show tasks whose completed prop is false
+  Active: task => !task.completed,
+  // shows tasks whose completed prop is true
+  Completed: task => task.completed
+};
+
+// using the Object.keys method to collect an array of FILTER_NAMES;
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 export default function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   // counting the length of tasks and changing the text of our heading accordingly
   const tasksNoun = tasks.length !== 1 ? 'tasks' : 'task';
   const headingText = `${tasks.length} ${tasksNoun} remaining`;
 
   function toggleTaskCompleted(id) {
+    // console.log(tasks[0]); test the method
     const updatedTasks = tasks.map(task => {
       if (id === tasks.id) {
         return {...task, completed: !task.completed}
@@ -21,8 +37,25 @@ export default function App(props) {
   }
 
   function deleteTask(id) {
+    // console.log(id);
     const remainingTasks = tasks.filter(task => id!== task.id);
     setTasks(remainingTasks);
+  }
+
+  // pass addTask into Form as prop
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editTaskList = tasks.map(task => {
+      if (id == task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editTaskList);
   }
 
   // iterate Todo and store it at taskList
@@ -34,15 +67,10 @@ export default function App(props) {
           key={task.id}
           toggleTaskCompleted={toggleTaskCompleted}
           deleteTask={deleteTask}
+          editTask={editTask}
         />
       )
     );
-
-  // pass addTask into Form as prop
-  function addTask(name) {
-    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
-    setTasks([...tasks, newTask]);
-  }
 
   return (
     <div className="todoapp stack-large">
--- components/FilterButton.js
+++ components/FilterButton.js
@@ -5,7 +5,7 @@ export default function FilterButton(props) {
     <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
           <span className="visually-hidden">Show </span>
-          <span>all</span>
+          <span>All</span>
           <span className="visually-hidden"> tasks</span>
         </button>
         <button type="button" className="btn toggle-btn" aria-pressed="false">
